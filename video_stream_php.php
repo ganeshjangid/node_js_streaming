8eb423702a90a9c4b95b469fcd4b554595e28194
ganeshjangid <webdeveloper.ganesh@gmail.com>
2019-09-04 12:57:26 +0530

    te

---
+++ video_stream_php.php
@@ -0,0 +1,11 @@
+<video width="320" height="240" controls="">
+  <source src="./assets/sample.mp4" type="video/mp4">
+Your browser does not support the video tag.
+</source></video>
+
+
+
+<!-- 
+http://project01.ganeshsuthar.mum.jdsoftware.com/ganeshsuthar_dev/code/node/small_project/streaming/video_stream_php.php
+
+ -->
